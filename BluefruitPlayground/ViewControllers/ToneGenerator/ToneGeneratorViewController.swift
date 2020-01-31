e4ca5d4b2e6e5ba90a1b6ce0596e64737368f873
Antonio <antonio@openroad.es>
2020-01-31 11:32:20 +0100

    ToneGenerator: move keyboard bounds away from edges

--- BluefruitPlayground/ViewControllers/ToneGenerator/ToneGeneratorViewController.swift
+++ BluefruitPlayground/ViewControllers/ToneGenerator/ToneGeneratorViewController.swift
@@ -46,6 +46,10 @@ class ToneGeneratorViewController: UIViewController {
     @IBOutlet weak var keysContainerView: UIView!
     @IBOutlet weak var whiteKeysStackView: UIStackView!
     @IBOutlet weak var speakerImageView: UIImageView!
+    @IBOutlet weak var halfKeyTop: UIButton!
+    @IBOutlet weak var halfKeyBottom: UIButton!
+    @IBOutlet weak var extraHalfKeyTop: UIButton!
+    @IBOutlet weak var extraHalfKeyBottom: UIButton!
     
     // Data
     private var tonesPlaying = Set<Int>()
@@ -75,25 +79,49 @@ class ToneGeneratorViewController: UIViewController {
             }
         }
 
+        // - Add 2 extra hidden buttons to extend the touch area of the black half-keys
+        
+        for button in [extraHalfKeyTop!, extraHalfKeyBottom!] {
+         button.addTarget(self, action: #selector(redirectHalfKeyDown(_:)), for: .touchDown)
+         button.addTarget(self, action: #selector(redirectHalfKeyUp(_:)), for: [.touchUpInside, .touchUpOutside, .touchCancel])
+         }
+        
+        // - Round the container so the half-keys that are clipped look rounded too
+        keysContainerView.layer.cornerRadius = 4
+        keysContainerView.layer.masksToBounds = true
+        
         // Localization
         let localizationManager = LocalizationManager.shared
         self.title = localizationManager.localizedString("tonegenerator_title")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-           super.viewWillAppear(animated)
-           
-           // Navigationbar setup
-           if let customNavigationBar = navigationController?.navigationBar as? NavigationBarWithScrollAwareRightButton {
-               customNavigationBar.setRightButton(topViewController: self, image: UIImage(named: "help"), target: self, action: #selector(help(_:)))
-           }
-       }
+        super.viewWillAppear(animated)
+        
+        // Navigationbar setup
+        if let customNavigationBar = navigationController?.navigationBar as? NavigationBarWithScrollAwareRightButton {
+            customNavigationBar.setRightButton(topViewController: self, image: UIImage(named: "help"), target: self, action: #selector(help(_:)))
+        }
+    }
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Disable the pop recognizer to avoid problems with keys on the edges
+        self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    override var preferredScreenEdgesDeferringSystemGestures: UIRectEdge {
+        // Remove the system gestures from left and right edges
+        return [.top, .bottom]       // not left or right
+    }
+    
     
     // MARK: - Actions
     @objc private func keyDown(_ sender: UIButton) {
         let tag = sender.tag
         tonesPlaying.insert(tag)
-        
+
         // UI Animation
         UIView.animate(withDuration: 0.2) {
             self.speakerImageView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
@@ -108,7 +136,7 @@ class ToneGeneratorViewController: UIViewController {
     @objc private func keyUp(_ sender: UIButton) {
         let tag = sender.tag
         tonesPlaying.remove(tag)
-        
+
         // UI Animation
         if tonesPlaying.isEmpty {
             UIView.animate(withDuration: 0.15) {
@@ -123,6 +151,18 @@ class ToneGeneratorViewController: UIViewController {
         else {
             CPBBle.shared.toneGeneratorStopPlaying()
         }
+    }
+    
+    @objc private func redirectHalfKeyDown(_ sender: UIButton) {
+        let targetButton =  sender === extraHalfKeyTop ? halfKeyTop : halfKeyBottom
+        targetButton?.sendActions(for: .touchDown)
+        targetButton?.isHighlighted = true
+    }
+
+    @objc private func redirectHalfKeyUp(_ sender: UIButton) {
+        let targetButton =  sender === extraHalfKeyTop ? halfKeyTop : halfKeyBottom
+        targetButton?.sendActions(for: .touchUpInside)
+        targetButton?.isHighlighted = false
     }
     
     private func frequencyForKeyTag(_ tag: Int) -> Double? {
@@ -150,3 +190,4 @@ class ToneGeneratorViewController: UIViewController {
         self.present(navigationController, animated: true, completion: nil)
     }
 }
+
