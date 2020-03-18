0d33821c88dc547f3cdfa67410392f2d60b1b098
Antonio <antonio@openroad.es>
2020-03-18 13:03:05 +0100

    Fixed text links Updated strings

--- BluefruitPlayground/Utils/ActiveLabelUtils.swift
+++ BluefruitPlayground/Utils/ActiveLabelUtils.swift
@@ -10,12 +10,12 @@ import UIKit
 import ActiveLabel
 
 struct ActiveLabelUtils {
+    
     public static func addActiveLabelLinks(label: ActiveLabel, linksLocalizationStringsIdPrefix: String) {
         let localizationManager = LocalizationManager.shared
         label.text  = localizationManager.localizedString("\(linksLocalizationStringsIdPrefix)_text")
         
         label.customize { label in
-            //let kMaxLinksNumber = 10        // Maximum number of links
             let color = UIColor(named: "text_link") ?? .blue
             let selectedColor = color.lighter()
             
@@ -23,35 +23,24 @@ struct ActiveLabelUtils {
             label.URLColor = color
             label.URLSelectedColor = selectedColor
             
-            // Search links
+            // Search general link
+            addLink(label: label, linkPrefix: "\(linksLocalizationStringsIdPrefix)_link")
+            
+            // Search numbered links
             var found = true
             var i = 0
             repeat {
-                if let linkString = localizationManager.localizedStringIfExists("\(linksLocalizationStringsIdPrefix)_link\(i)_text") {
-                    
-                    let customType = ActiveType.custom(pattern: "(\\w*\(linkString)\\w*)")
-                    label.enabledTypes.append(customType)
-                    
-                    label.customColor[customType] = color
-                    label.customSelectedColor[customType] = selectedColor
-                    
-                    if let linkUrl = URL(string: localizationManager.localizedString("\(linksLocalizationStringsIdPrefix)_link\(i)_url")) {
-                        label.handleCustomTap(for: customType) { _ in
-                            UIApplication.shared.open(linkUrl, options: [:], completionHandler: nil)
-                        }
-                    }
-                    
-                    i = i + 1
-                }
-                else {
-                    found = false
-                }
-            } while(found/* && i < kMaxLinksNumber*/)
+                found = addLink(label: label, linkPrefix: "\(linksLocalizationStringsIdPrefix)_link\(i)")
+                i = i + 1
+
+            } while(found)
             
+            // Add handle for text urls
             label.handleURLTap { url in
                 UIApplication.shared.open(url, options: [:], completionHandler: nil)
             }
             
+            // Modify link style
             label.configureLinkAttribute = { (type, attributes, isSelected) in
                 var atts = attributes
                 if label.enabledTypes.contains(type) {
@@ -60,5 +49,31 @@ struct ActiveLabelUtils {
                 return atts
             }
         }
+    }
+    
+    /**
+        Add link to an ActiveLabel
+        returns: true if a link matching the text is found on localization strings
+     */
+    @discardableResult
+    private static func addLink(label: ActiveLabel, linkPrefix: String) -> Bool {
+        let localizationManager = LocalizationManager.shared
+        guard let linkString = localizationManager.localizedStringIfExists("\(linkPrefix)_text") else { return false }
+        let customType = ActiveType.custom(pattern: "(\\w*\(linkString)\\w*)")
+        label.enabledTypes.append(customType)
+        
+        let color = UIColor(named: "text_link") ?? .blue
+        let selectedColor = color.lighter()
+        
+        label.customColor[customType] = color
+        label.customSelectedColor[customType] = selectedColor
+        
+        if let linkUrl = URL(string: localizationManager.localizedString("\(linkPrefix)_url")) {
+            label.handleCustomTap(for: customType) { _ in
+                UIApplication.shared.open(linkUrl, options: [:], completionHandler: nil)
+            }
+        }
+        
+        return true
     }
 }
